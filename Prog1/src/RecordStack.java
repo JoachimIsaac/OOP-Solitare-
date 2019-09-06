8c8ca1c29cdf797fed202aa6694c25532fcf1e5f
Joachim Isaac <joachimvivian3@gmail.com>
2019-09-06 15:00:45 -0500

    Add files via upload

---
+++ Prog1/src/RecordStack.java
@@ -0,0 +1,67 @@
+public class RecordStack {
+    //Stack using linked list
+
+    //Linked list node
+    private class Node{
+        private String val;
+        private Node next = null;
+        private Node(String data){
+            this.val = data;
+        }
+    }
+
+    //Top reference variable.
+   private Node top;
+    //Property to keep track of stack contents
+   private int stack_amount;
+
+    //Constructor
+    RecordStack(){
+        this.top = null;
+        this.stack_amount = 0;
+    }
+
+    public void push(String card){
+
+        Node node = new Node(card);
+
+           if(node == null) {
+               System.out.println("\n Stack overFlow");
+               return;
+           }
+           node.next = top;
+           top = node;
+           stack_amount += 1;
+    }
+
+    public boolean isEmpty(){
+
+        //If top points to null then it is empty if it doesn't point to null then it has contents.
+        return this.top == null;
+    }
+
+    public String peek(){
+
+        if(!isEmpty()){
+            return top.val;
+        }
+        else {
+            System.out.println("The Stack is Empty");
+            return null;
+        }
+    }
+
+
+    public String pop(){
+
+        if(top == null){
+            System.out.println("\nStack Underflow!");
+        }
+
+        String data = top.val;
+        top = top.next;
+        return data;
+    }
+
+
+}
