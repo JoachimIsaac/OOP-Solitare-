4c9d707608d523083ba3992a058fec48256e976e
Joachim Isaac <joachimvivian3@gmail.com>
2019-09-09 13:57:51 -0500

    Add files via upload

--- Prog1/src/Prog1.java
+++ Prog1/src/Prog1.java
@@ -10,35 +10,34 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Prog1 {
 
-    public static final int DECK_SIZE = 52;
+//    public static final int DECK_SIZE = 52;
 
     public static void main(String args[]) throws IOException {
 
 
-            int set_number = 1;
+            int set_number = 1; //number of games ran essentially
             int number_of_cards = 0;
 
 
             System.out.println("Name: Joachim Isaac\nProgram1: Solitaire Stacks\n");
-            String copy_of_cards = read_in_cards(getinputFile());
+            String copy_of_cards = read_in_cards(getinputFile()); //Reads in the string of cards and puts it into a variable.
             File output_file = getoutFile();
-            number_of_cards = getnumber_of_cards(copy_of_cards);
+            number_of_cards = getnumber_of_cards(copy_of_cards);//Gets the number of cards we have
             ArrayList<RecordStack> card_stack = new ArrayList<RecordStack>(number_of_cards);
 
-            card_stack = set_cards(set_stacks(card_stack, number_of_cards), copy_of_cards);
-            card_stack = play(card_stack);
+            card_stack = set_cards(set_stacks(card_stack, number_of_cards), copy_of_cards);//Sets the stacks and then sets the cards within the seperate stacks
+            card_stack = play(card_stack);//Game logic to manipulate the stacks and it returns the array of stack that is changed.
 
-            print_results(card_stack, output_file, copy_of_cards, set_number);
+            print_results(card_stack, output_file, copy_of_cards, set_number);//Results from the game are printed
             set_number++;
 
 
-        // Display what is remaining from the play , i.e results
-
-
-        //Figure out how ro make it print two sets.
+       //Look into ensuring that the reading in can work in many different ways(Question: in the instructions you
+        // said it("#") ends on the line so are there cases where it could be in the middle of the array?)
 
 
     }
@@ -47,7 +46,7 @@ public class Prog1 {
     public static File getinputFile() {
 
         //Gets the input file name and stores it in a variable.
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in).useDelimiter("\\s*fish\\s*");
         System.out.println("Enter the input file name:");
         String input_file_name = input.nextLine();
         File input_file = new File(input_file_name.strip());
@@ -56,7 +55,7 @@ public class Prog1 {
     }
 
     public static File getoutFile() {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in).useDelimiter("\\s*fish\\s*");
 //      Gets the output file name and stores it in a variable.
         System.out.println("Enter the output file name:");
         String output_file_name = input.nextLine();
@@ -69,7 +68,7 @@ public class Prog1 {
     }
 
 
-    public static String read_in_cards(File input_file_name) {
+    public static String read_in_cards(File input_file_name) { //Issues here
         try {
             //Works with file object -->input_file<--
             //In this case it will be reading from the input file rather than from the console/terminal.
@@ -84,7 +83,10 @@ public class Prog1 {
                 line = infile.nextLine();
 
                 if (line.charAt(0) != '#') {//
-                    cards += "\n" + line;
+                    cards += "\n"+ line;
+                    //add the "+ /n"
+
+                    System.out.println(line);
                 }
 
                 line = "";
@@ -99,16 +101,30 @@ public class Prog1 {
     }
 
 
-    public static int getnumber_of_cards(String cards) {
-        //Name delimiters something else later.
-        String delims = "[ ]+";
-//        String array to put the cards into
-// change tokens name --> better naming needed. But it is an array o strings that contains each card in a
-        // index thanks to split.(delims);
-        String[] tokens = cards.split(delims);
+//    public static int getnumber_of_cards(String cards) {
+//        //Name delimiters something else later.
+//        String delims = "[ ]+";
+//
+////        String array to put the cards into
+//// change tokens name --> better naming needed. But it is an array o strings that contains each card in a
+//        // index thanks to split.(delims);
+//        String[] tokens = cards.split(delims);
+//
+//        System.out.println("Number in string: " + tokens.length);
+//        return tokens.length;
+//    }
 
-        System.out.println("Number in string: " + tokens.length);
-        return tokens.length;
+    public static int getnumber_of_cards(String cards) { //tokenizer apparently fixed the issue
+        int counter  = 0;
+        StringTokenizer cards_to_count = new StringTokenizer(cards);
+
+        for (int i = 0; cards_to_count.hasMoreTokens(); i++) {
+
+            cards_to_count.nextToken();
+            counter++;
+        }
+
+        return counter;
     }
 
     public static ArrayList<RecordStack> set_stacks(ArrayList card_stack, int number_of_cards) {
@@ -122,13 +138,26 @@ public class Prog1 {
     }
 
 
-    public static ArrayList<RecordStack> set_cards(ArrayList<RecordStack> card_stack, String cards) {
-        String delims = "[ ]+";
-        String[] tokens = cards.split(delims);
+//    public static ArrayList<RecordStack> set_cards(ArrayList<RecordStack> card_stack, String cards) {
+//        String delims = "[ ]+";
+//        String[] tokens = cards.split(delims);
+//
+//        for (int i = 0; i < tokens.length; i++) {
+//            card_stack.get(i).push(tokens[i]);
+//        }
+//        return card_stack;
+//    }
 
-        for (int i = 0; i < tokens.length; i++) {
-            card_stack.get(i).push(tokens[i]);
+
+    public static ArrayList<RecordStack> set_cards(ArrayList<RecordStack> card_stack, String cards) {
+        StringTokenizer cards_to_input = new StringTokenizer(cards);
+
+ for (int i = 0; cards_to_input.hasMoreTokens(); i++) {
+            card_stack.get(i).push(cards_to_input.nextToken());
+//        System.out.println(cards_to_input.nextToken());
         }
+
+
         return card_stack;
     }
 
@@ -137,10 +166,14 @@ public class Prog1 {
         Boolean gap_closed = false;
 
 
-        //go over the logic again but for now it seems almost done !
+        //go over the logic again but for now it seems like it should work< issue with the array .
         while (card_pointer < card_stacks.size()) {
-            System.out.println(check_1_left(card_stacks, card_pointer));
-            System.out.println(card_stacks.get(0).peek().charAt(1));
+            System.out.println("\n"); //Comment everything from here let it run without errors
+            System.out.println("Check one from left "+ check_1_left(card_stacks, card_pointer));
+            System.out.println("Check three from left " + check_3_left(card_stacks, card_pointer));
+            System.out.println("The array which contains the stacks' size:--> " + card_stacks.size());
+            System.out.println("Length of card away from pointer(-1): " + card_stacks.get(card_pointer - 1).peek().length());
+            System.out.println("Length of card away from pointer(-3): " + card_stacks.get(card_pointer - 1).peek().length() + "\n");
 
             if (card_pointer >= 3 && check_3_left(card_stacks, card_pointer)) {
                 String card_to_move = card_stacks.get(card_pointer).pop();
@@ -226,7 +259,7 @@ public class Prog1 {
             amount_in_piles += " " + Integer.toString(current_amount);
         }
 
-        try {//Output to txt file , will
+        try {//Output to txt file
             PrintWriter output = new PrintWriter(output_file_name);
             output.println("Set " + set_number + ":");
             output.println(cards);
